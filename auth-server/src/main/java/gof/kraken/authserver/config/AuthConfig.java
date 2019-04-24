acd838b9bb927f7abb6b80f547eda2fe2cb2116d
Alex Vidal <arodriguez@tribalscale.com>
2019-04-24 17:14:57 +0400

    resource server and JWT

--- auth-server/src/main/java/gof/kraken/authserver/config/AuthConfig.java
+++ auth-server/src/main/java/gof/kraken/authserver/config/AuthConfig.java
@@ -1,9 +1,9 @@
 package gof.kraken.authserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,7 +12,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
 
 @Configuration
 @EnableAuthorizationServer
@@ -20,9 +24,6 @@ public class AuthConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private TokenStore tokenStore;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -35,18 +36,27 @@ public class AuthConfig extends AuthorizationServerConfigurerAdapter {
                 .scopes("read", "write")
                 .autoApprove(true)
                 .secret(encoder.encode("password"));
+//                .redirectUris("http://localhost:9999/login"); TODO check what happens with this redirect uri
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints
                 .authenticationManager(authenticationManager)
-                .tokenStore(tokenStore);
+                .accessTokenConverter(accessTokenConverter())
+                .tokenStore(tokenStore());
     }
 
     @Bean
     public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
 
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource("keys/mytest.jks"), "mypass".toCharArray()).getKeyPair("mytest");
+        converter.setKeyPair(keyPair);
+        return converter;
+    }
 }
