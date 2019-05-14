57bcb02b341fc6f0fdb79b0f5991727370f6e5aa
Alex Vidal <arodriguez@tribalscale.com>
2019-05-14 12:09:02 +0400

    working auth flow in docker and proxy forwarding

--- gateway-service/src/main/java/gof/kraken/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/gof/kraken/gatewayservice/GatewayServiceApplication.java
@@ -4,6 +4,7 @@ import gof.kraken.gatewayservice.config.AuthHeaderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -17,6 +18,12 @@ public class GatewayServiceApplication {
     @Bean
     public AuthHeaderFilter authorizationHeaderFilter() {
         return new AuthHeaderFilter();
+    }
+
+    //LocationRewrite handles 3XX from end-points avoiding a redirect to them
+    @Bean
+    public LocationRewriteFilter locationRewriteFilter() {
+        return new LocationRewriteFilter();
     }
 
 }
--- user-service/src/main/java/gof/kraken/userservice/config/ResourceServerConfig.java
+++ user-service/src/main/java/gof/kraken/userservice/config/ResourceServerConfig.java
@@ -1,6 +1,5 @@
 package gof.kraken.userservice.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,23 +7,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableResourceServer
-//@RestController
 @Configuration
-//@EnableOAuth2Sso
-//@Order(value = 10)
+@Order(value = 1) //Needs to be order lower than 3 to kick in before the @EnableResource config
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/login").permitAll()
-                .and().authorizeRequests().anyRequest().authenticated();
-
-//        http.requestMatchers().antMatchers("/private") // Deny access to "/ private"
-//                .and().authorizeRequests()
-//                .antMatchers("/private").access("hasRole ('USER')")
-//                .and()
-//                .requestMatchers().antMatchers("/admin") // Deny access to "/ admin"
-//                .and()
-//                .authorizeRequests().antMatchers("/admin").access("hasRole('ADMIN')");
+        http
+                .requestMatchers().antMatchers("/")
+                .and().authorizeRequests()
+                .antMatchers("/private").authenticated();
     }
 }
