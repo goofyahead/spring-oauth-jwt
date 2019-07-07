60e132a592d2de6c41db664ae35c4057c3082341
goofyahead <goofyahead@TSDXB034s-iMac.local>
2019-07-07 15:10:22 +0400

    test: 💍 Integration test with Token Auth Check
    Sample integration test for both m2m and user flows.

--- user-service/src/main/java/gof/kraken/userservice/config/ResourceServerConfig.java
+++ user-service/src/main/java/gof/kraken/userservice/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package gof.kraken.userservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
--- user-service/src/main/java/gof/kraken/userservice/controller/UserController.java
+++ user-service/src/main/java/gof/kraken/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package gof.kraken.userservice.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,15 @@ public class UserController {
         return "Wow you are allowed here? Hello " + user.getName();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping("/admin")
     public String adminPage(Principal user) {
         return "GASP! you are and ADMIN? Hello " + user.getName();
+    }
+
+    @PreAuthorize("hasAuthority('ARTICLES_READ')")
+    @RequestMapping("/articles")
+    public String permissionNeeded(Principal user) {
+        return "Ok, you can read your articles " + user.getName();
     }
 }
